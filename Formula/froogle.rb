b5ef5634bc505ac5a41263d790c03cc6303b8158
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2024-06-15 00:01:53 -0700

    Updated SHA for v0.3.0

--- Formula/froogle.rb
+++ Formula/froogle.rb
@@ -2,9 +2,9 @@ class Froogle < Formula
     desc "Local Search Engine using TF-IDF"
     homepage "https://github.com/tripabhi/froogle"
     
-    url "https://github.com/tripabhi/froogle/releases/download/v0.2.0/froogle-0.2.0-aarch64-apple-darwin.tar.gz"
-    sha256 "3b22d843233c67fe198ccd14e495ab90fa64493f38a519241c956ecc3b203695"
-    version "0.2.0"
+    url "https://github.com/tripabhi/froogle/releases/download/v0.3.0/froogle-0.3.0-aarch64-apple-darwin.tar.gz"
+    sha256 "f578a2d565fea8c8ee9246fa414bfb3c841644ca1fffa115d8cb622f1e2670e1"
+    version "0.3.0"
   
     def install
       bin.install "froogle"
