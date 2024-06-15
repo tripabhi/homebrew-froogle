368fae35a6b237b6212d7fd6a6cb47b1530d09a7
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2024-06-14 23:51:07 -0700

    Updated SHA for latest version

--- Formula/froogle.rb
+++ Formula/froogle.rb
@@ -3,7 +3,7 @@ class Froogle < Formula
     homepage "https://github.com/tripabhi/froogle"
     
     url "https://github.com/tripabhi/froogle/releases/download/v0.2.0/froogle-0.2.0-aarch64-apple-darwin.tar.gz"
-    sha256 "c89d4b82f64e60bb523c50e7c7f7bd92c20249e87f28e8e521389aa4cc85c17b"
+    sha256 "3b22d843233c67fe198ccd14e495ab90fa64493f38a519241c956ecc3b203695"
     version "0.2.0"
   
     def install
