69d3643cff06316168139844e53ba6a64ca493a4
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2024-06-15 13:16:57 -0700

    Update to Froogle v0.4.2

--- Formula/froogle.rb
+++ Formula/froogle.rb
@@ -1,12 +1,23 @@
 class Froogle < Formula
     desc "Local Search Engine using TF-IDF"
     homepage "https://github.com/tripabhi/froogle"
-    
-    url "https://github.com/tripabhi/froogle/releases/download/v0.4.1/froogle-aarch64-apple-darwin.tar.xz"
-    sha256 "e5b0d595b7e426f28c857ddd1fd64b2ed2aba1121c0f2ba08d37a82ec579d5ef"
-    version "0.4.1"
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/tripabhi/froogle/releases/download/v0.4.2/froogle-aarch64-apple-darwin.tar.xz"
+        sha256 "8176aa990fb5df2a9269f8f8e445b9661a1278abc19b5874cef1133da479bd26"
+      elsif Hardware::CPU.intel?
+        url "https://github.com/tripabhi/froogle/releases/download/v0.4.2/froogle-x86_64-apple-darwin.tar.xz"
+        sha256 "85d74a4caed1fb721f49d1b9ab0f01b9ac35cb8da925b90e38e94f7654161704"
+      end
+    end
+    version "0.4.2"
   
     def install
       bin.install "froogle"
     end
+  
+    test do
+      system "#{bin}/froogle", "--version"
+    end
+  end
   end
