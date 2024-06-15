257fdcfce0e587fad658d6a38ff0c9aa2b17f8ee
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2024-06-15 00:08:54 -0700

    Updated Froogle version to v0.4.0

--- Formula/froogle.rb
+++ Formula/froogle.rb
@@ -4,7 +4,7 @@ class Froogle < Formula
     
     url "https://github.com/tripabhi/froogle/releases/download/v0.4.0/froogle-0.4.0-aarch64-apple-darwin.tar.gz"
     sha256 "f2415274b597186bc97d7c009ddba03b42f0da953ec2e570717bd9e64ca8b07f"
-    version "0.3.0"
+    version "0.4.0"
   
     def install
       bin.install "froogle"
